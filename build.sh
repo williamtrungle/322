1aa39a348284c81d59b7bb7d9110371d7d25278a
shrike <williamtrung.le@gmail.com>
2018-03-07 00:09:56 -0500

    Q3

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 if [ "$1" = "1" ]; then
-    rm src/matrix
+    rm src/matrix 2>/dev/null
     g++ src/a1.cpp -o src/matrix
 elif [ "$1" = "2" ]; then
-    rm src/dll
+    rm src/dll 2>/dev/null
     g++ src/a2.cpp -o src/dll
 else
     echo Error: accepted arguments are "1" "2"
--- src/a2.cpp
+++ src/a2.cpp
@@ -6,24 +6,67 @@
 using namespace std;
 
 class Node {
-    public:
-        int data;
-        Node *prev;
-        Node *next;
-        Node(int d, Node *p, Node *n) {
-            data = d;
-            prev = p;
-            next = n;
+  public:
+    int data;
+    Node *prev;
+    Node *next;
+    Node(int d, Node *p, Node *n) {
+        data = d;
+        prev = p;
+        next = n;
+    }
+    Node() {
+        data = 0;
+        prev = next = NULL;
+    }
+    ~Node() {}
+};
+
+class DLLStructure {
+  private:
+    Node *first, *last;
+  public:
+    DLLStructure() {
+        first = last = NULL;
+    }
+    DLLStructure(int array[], int size) {
+        int i = 0;
+        Node *c = NULL;
+        Node *p = NULL;
+        while (i < size) {
+            c = new Node(array[i], p, NULL);
+            if (i > 0) {
+                p->next = c;
+            } else {
+                first = c;
+            }
+            p = c;
+            i++;
         }
-        Node() {
-            data = 0;
-            prev = NULL;
-            next = NULL;
+        last = c;
+    }
+    ~DLLStructure() {
+        Node *c = first;
+        Node *n;
+        while (c != last) {
+            n = c->next;
+            delete c;
+            c = n;
         }
-        ~Node() {}
+    }
+    void printDLL() {
+        Node *c = first;
+        Node *n;
+        do {
+            cout << c->data << " ";
+        } while ((c = c->next));
+        cout << endl;
+    }
 };
 
 int main() {
-    Node node();
+    int array[5] = {11,2,7,22,4};
+    DLLStructure dll(array,5);
+    dll.printDLL();
     return 0;
 }
