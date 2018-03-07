b5862e46fe28d6e2f65b86a2859dd2312220ffe4
shrike <williamtrung.le@gmail.com>
2018-03-07 15:53:26 -0500

    Q12

--- src/a2.cpp
+++ src/a2.cpp
@@ -64,10 +64,12 @@ class DLLStructure {
     void InsertAfter(int after, int value) {
         Node *c = first;
         Node *n;
-        while (c != last) {
+        while (c != NULL) {
             if (c->data == after) {
                 n = new Node(value, c, c->next);
-                c->next->prev = n;
+                if (c->next != NULL) {
+                    c->next->prev = n;
+                }
                 c->next = n;
                 break;
             }
@@ -163,18 +165,37 @@ class DLLStructure {
         }
         return m;
     }
-
+    DLLStructure(const DLLStructure &other) {
+        if ((!other.first)) {
+            first = last = NULL;
+        } else {
+            Node *c = NULL;
+            Node *o = other.first;
+            first = new Node(o->data, NULL, NULL);
+            Node *p = first;
+            while ((o = o->next)) {
+                c = new Node(o->data, p, NULL);
+                p->next = c;
+                p = c;
+            }
+            last = c;
+        }
+    }
 };
 
 // ----------------------------------------------------------------------------
-// Question 10 (5pts)
+// Question 10 (5 pts)
 
 string q10 = "Ideally the value of the size of the list would be saved as a private field of the class, and be updated on every insertion/deletion method. This would reduce the O(n) size lookup downt to O(1).";
 
 // ----------------------------------------------------------------------------
-// Question 11 (10pts)
+// Question 11 (10 pts)
 
 string q11 = "Similar to the GetSize case, the value of max and min should be saved as a field of the object, and be updated whenever an element is added or removed. In those case, we modify either value by simply comparing with the current max and min, which has a O(1) time complexity.";
+
+// ----------------------------------------------------------------------------
+// Question 12 (15 pts)
+string q12 = "If we only rely on the implicit copy constructor provided by the compiler, the resulting object will have a copy of all of the fields of the original object. What this entails is that both objects point to the same list! Therefore a modification in our newly created object will unexpectedly modify the contents of the original object. This is not the behaviour we want. Thus, we require an explicity copy structor method, which will perform a deep copy of the original method. Implementing this requires simply to traverse the original list and make a copy of each node for the new copied DLLStructure object.";
 
 int main() {
     int array[5] = {11,2,7,22,4};
@@ -206,5 +227,14 @@ int main() {
     } else {
         cout << "Not empty :(" << endl;
     }
+    cout << "Copy lis, insert 666 before 26, and check that original and new are different" << endl;
+    DLLStructure newList = dll;
+    newList.InsertBefore(26, 666);
+    cout << "Original:\t";
+    dll.PrintDLL();
+    cout << "New:\t\t";
+    newList.PrintDLL();
+    cout << "Question 12:" << endl << q12 << endl;
+
     return 0;
 }
